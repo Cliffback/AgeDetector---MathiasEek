f88d2214c48ad7f95dc9b46f47777b6022b9b1ca
Mathias <Cliffback@users.noreply.github.com>
2020-09-01 14:18:45 +0200

    Add project files.

---
+++ 200901 Task_age/200901 Task_age.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <string>
+#include <conio.h>
+
+int age;
+
+int main()
+{
+	system("cls");
+	std::cout << "What is your age?\n";
+	std::cin >> age;
+	if (age <= 20)
+	{
+		std::cout << "You are an adult\n";
+	}
+	else
+	{
+		std::cout << "\nYou are certainly a teenager!\n";
+	}
+	char something = _getch();
+	return 0;
+}
+
+ 
